717a52d08ecd2664a6b3d8099bff5efae20b6bd1
Smey <roemreaksmey7@gmail.com>
2024-08-21 14:14:41 +0700

    fix site

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "babel",
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
-  // Other Jest options
-};
